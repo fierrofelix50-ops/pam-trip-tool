16fdb943da2a1ea28eb01ff7fcc4a6fa6e1b03b9
fierr <fierr@example.com>
2026-02-22 00:02:52 -0700

    Update Trip Tool to v6 with PWA support

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'trip-tool-v2';
+const assets = [
+    './',
+    './index.html',
+    './4012.jpg',
+    './4013.jpg',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(assets))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
